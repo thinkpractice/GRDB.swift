65a6d22bdf06a4d6697bf688dcdcb682292f8441
Tim De Jong <gitlab@thinkpractice.nl>
2025-05-24 08:39:43 +0200

    Fixed indentation

--- Tests/GRDBTests/CGFloatTests.swift
+++ Tests/GRDBTests/CGFloatTests.swift
@@ -4,21 +4,20 @@ import GRDB
 import XCTest
 
 class CGFloatTests: GRDBTestCase {
+    func testCGFLoat() throws {
+        let dbQueue = try makeDatabaseQueue()
+        try dbQueue.inDatabase { db in
+            try db.execute(sql: "CREATE TABLE points (x DOUBLE, y DOUBLE)")
 
-  func testCGFLoat() throws {
-    let dbQueue = try makeDatabaseQueue()
-    try dbQueue.inDatabase { db in
-      try db.execute(sql: "CREATE TABLE points (x DOUBLE, y DOUBLE)")
+            let x: CGFloat = 1
+            let y: CGFloat? = nil
+            try db.execute(sql: "INSERT INTO points VALUES (?,?)", arguments: [x, y])
 
-      let x: CGFloat = 1
-      let y: CGFloat? = nil
-      try db.execute(sql: "INSERT INTO points VALUES (?,?)", arguments: [x, y])
-
-      let row = try Row.fetchOne(db, sql: "SELECT * FROM points")!
-      let fetchedX: CGFloat = row["x"]
-      let fetchedY: CGFloat? = row["y"]
-      XCTAssertEqual(x, fetchedX)
-      XCTAssertTrue(fetchedY == nil)
+            let row = try Row.fetchOne(db, sql: "SELECT * FROM points")!
+            let fetchedX: CGFloat = row["x"]
+            let fetchedY: CGFloat? = row["y"]
+            XCTAssertEqual(x, fetchedX)
+            XCTAssertTrue(fetchedY == nil)
+        }
     }
-  }
 }
